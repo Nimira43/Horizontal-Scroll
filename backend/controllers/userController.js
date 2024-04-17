a9c9824b4299b4e0d78312aca10ce681df134f8e
Nimira43 <n.rabone72@aol.com>
2024-04-18 00:32:28 +0100

    Error handler added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,10 @@
-const authUser = (req, res) => {
+import asyncHandler from 'express-async-handler'
+
+const authUser = asyncHandler(async (req, res) => {
+    res.status(401)
+    throw new Error('Somethin went wrong')
     res.status(200).json({ message: 'Auth user' })
-}
+})
 
 export {
     authUser
